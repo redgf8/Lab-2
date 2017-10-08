d3b47ff9645e7d789220efedddda158b60bd2698
Xmodious <32528798+Xmodious@users.noreply.github.com>
2017-10-08 18:59:53 -0400

    Add files via upload

--- src/MyList_Array.java
+++ src/MyList_Array.java
@@ -1,36 +1,60 @@
 
 public class MyList_Array<T> implements MyList {
 	
-	private T[] list;
+	public T[] list;
 	
-	private int MaxCapacity, CurrentSize;
+	private int ArraySize, capacity;
+	
+	private static final int INITIAL_CAPACITY = 10;
 	
 	public MyList_Array() {
-		MaxCapacity = 10;
-		list = (T[]) new Object[MaxCapacity];
+		capacity = INITIAL_CAPACITY;
+		ArraySize = 0;
+	}
+	
+	public MyList_Array(int MaxCapcity) {
+		this.capacity = capacity;
+		this.ArraySize = 0;
+		list = (T[]) new Object[this.capacity];
+	}
+	
+	private void reallocate() {
+		this.capacity *= 2;
+		
+		T[] newList = (T[])new Object[this.capacity];
+		for(int i = 0; i < this.ArraySize; i++) {
+			newList[i] = list[i];
+		}
+		
+		this.list = newList;
+		
 	}
 	
 	public boolean add(int index, T o) {
 		
-		if(index < 0 || index > MaxCapacity) {
+		if(index < 0 || index > ArraySize) {
 			System.out.println("Bad Index.");
 			return false;
 		}
-		else if(index == MaxCapacity) {
+		else if(index == ArraySize){
 			this.add(o);
+			return true;
 		}
 		
 		else {
-			if (this.CurrentSize == this.MaxCapacity) {
-				System.out.println("List Full.");
-				return false;
+			if (this.capacity == this.ArraySize) {
+				System.out.println("List Full: Reallocating");
+				this.reallocate();
+				return true;
 			}
 			
-			for (int i = MaxCapacity; i > index; i--) {
+			for (int i = ArraySize; i > index; i--) {
 				this.list[i] = this.list[i - 1];
+				return true;
 			}
 			
 			this.list[index] = o;
+			capacity++;
 		}
 		
 		return true;
@@ -40,17 +64,18 @@ public class MyList_Array<T> implements MyList {
 	public boolean add(T o) {
 		
 		
-		if(CurrentSize < MaxCapacity) {
-			list[CurrentSize] = o;
-			CurrentSize++;
+		if(ArraySize < capacity) {
+			list[ArraySize] = o;
+			ArraySize++;
 			return true;
 		}
 		
 		else {
-			System.out.println("List Full");
-			return false;
+			System.out.println("List Full: Reallocating");
+			this.reallocate();
+			this.add(o);
+			return true;
 		}
-		
 	}
 	
 	public boolean clear() {
@@ -79,7 +104,7 @@ public class MyList_Array<T> implements MyList {
 	
 	public boolean isEmpty() {
 		
-		if(CurrentSize != 0) {
+		if(capacity != 0) {
 			return false;
 		}
 		
@@ -89,30 +114,31 @@ public class MyList_Array<T> implements MyList {
 	
 	public T remove(int index) {
 		
-		if(index < 0 || index > MaxCapacity) {
+		if(index < 0 || index > ArraySize) {
 			System.out.println("Bad Index.");
 		}
-		else if(index == MaxCapacity) {
-			CurrentSize--;
+		else if(index == ArraySize) {
+			capacity--;
 		}
 		
 		else {
-			if (this.CurrentSize == this.MaxCapacity) {
-				CurrentSize--;
+			if (this.capacity == this.ArraySize) {
+				capacity--;
 			}
 			
-			for (int i = MaxCapacity; i > index; i--) {
+			for (int i = ArraySize; i > index; i--) {
 				this.list[i] = this.list[i - 1];
 			}
 			
 		}
 		
+		capacity--;
 		return this.remove(index);
 	}
 	
 	public T remove(T o) {
 		
-		CurrentSize--;
+		capacity--;
 		return this.remove(o);
 		
 	}
@@ -125,7 +151,7 @@ public class MyList_Array<T> implements MyList {
 	
 	public int size() {
 		
-		return CurrentSize;
+		return capacity;
 		
 	}
 	
