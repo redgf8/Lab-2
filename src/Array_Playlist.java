ca434c29b4a55306dfd43b30d00c10c47ead827b
Xmodious <32528798+Xmodious@users.noreply.github.com>
2017-10-11 16:55:18 -0400

    Add files via upload

--- src/Array_Playlist.java
+++ src/Array_Playlist.java
@@ -3,132 +3,95 @@ public class Array_Playlist {
 
   private String playlistName = "";
   private MyList_Array playlist = new MyList_Array();
+  private Song placeholder = new Song("", "", 0f);
   
   
-  public Array_Playlist(String Name) {
-    
+  public Array_Playlist(String Name) { //defines the name of the playList
     playlistName = Name;
-    
-    
   } 
   
-  String playlistName() {
-    
+  String playlistName() { //returns the name of the playlist
     return playlistName;
-    
   }
   
-  boolean addSong(Song s) {
-    
+  boolean addSong(Song s) { //Adds a song to the playlist
     playlist.add(s);
     return true;
-    
   }
   
-  boolean addSongAt(Song s, int index) {
-    
+  boolean addSongAt(int index, Song s) { //Adds a song to a specific spot in the playlist
     playlist.add(index, s);
     return true;
-    
   }
   
-  Song getSongAt(Song s, int index) {
-    
-    s = new Song("", "", 0);
+  Song getSongAt(Song s, int index) { //returns a song at a specific index
+    s = new Song("", "", 0f);
     s = (Song) playlist.get(index);
     return s;
-    
   }
   
-  MyList_Array getList() {
-    
+  MyList_Array getList() { //returns the playlist
     return playlist;
-    
   }
   
-  boolean removeSong(Song s) {
-    
+  boolean removeSong(Song s) { //removes a song from the playlist
     playlist.remove(s);
     return true;
-    
   }
   
-  int totalSongs() {
-    
+  int totalSongs() { //returns total number of song in the playlist
     return playlist.size();
-    
   }
   
-  float playlistTime() {
-    
+  float playlistTime(Song s) {  //returns duration of the entire playlist
     float totalTime = 0f;
-     Song placeholder = new Song("", "", 0);
      
-     for(int i = 0; i < totalSongs(); i++) {
-       placeholder = (Song) playlist.get(i + 1);
-       totalTime = (totalTime + placeholder.getPlayTime());
+     for(int i = 0; i < totalSongs(); i++) { //gets playtime of each song and adds them together
+       playlist.get(i);
+       totalTime = totalTime + s.getPlayTime();
      }
-     
      return totalTime;
-    
   }
   
-  boolean isSongInPlaylist(String name, String artist) {
-    
-    for(int i = 0; i < playlist.size(); i++) {
-      
+  boolean isSongInPlaylist(String name, String artist) { //returns true if there is a song in the playlist
+    for(int i = 0; i < playlist.size(); i++) {           //that contains the specified Artist and Song Name
       if(playlist.contains(artist) && playlist.contains(name)) {
-        
         return true;
-        
       }
-      
     }
-    
     return false;
-    
   }
   
-  void songsByArtist(String name) {
-    
-    Song placeholder = new Song("", "", 0);
-    
+  void songsByArtist(String artist) { //prints songs that contain a specified artist
+    Song placeholder = new Song("", "", 0f);
     
     for (int i = 0; i < totalSongs(); i++) {
-      
-      if(placeholder.getArtist().toLowerCase().equals(name.toLowerCase())) {
-        
+      if(placeholder.getArtist().toLowerCase().equals(artist.toLowerCase())) {
         System.out.println(placeholder.getSongName());
         placeholder = (Song) playlist.get(i + 1);
-        
-      }
-      else {
-        System.out.println("No songs by that artist. Please check spelling.");
+     }else {
+        System.out.println("No songs by that artist. Please check spelling."); //prints if no aritist exists
       }
     }
-    
   }
   
-  boolean addSongsFrom(Array_Playlist p) {
+  boolean addSongsFrom(Array_Playlist p) { //adds songs to playlist from another playlist
     Song placeholder = new Song("", "", 0);
     
     for(int i = 0; i < p.totalSongs(); i++) {
       playlist.add(p.getSongAt(placeholder, i + 1));
     }
     return true;
-    
   }
   
-  boolean MoveSong(Song s, int position){
-    
+  boolean MoveSong(Song s, int position){ //moves a song from a specific index, removes that index, and places it in another index
     int OriginalIndex = 0;
     OriginalIndex = playlist.indexOf(s);
     
     if(position < 1 || position > totalSongs()) {
       System.out.println("Invalid index.");
       return false;
-    }
-    else {
+   }else {
       playlist.add(position, s);
       playlist.remove(OriginalIndex);
       playlist.set(position, s);
@@ -137,10 +100,8 @@ public class Array_Playlist {
     }
   }
   
-  void MoveAllSongs(int positions){
-    
+  void MoveAllSongs(int positions){ //moves all songs by a specified amount
     playlist.shift(positions);
-    
   }
   
   
--- src/MyList.java
+++ src/MyList.java
@@ -1,34 +1,34 @@
 
 public interface MyList<T> {
 	
-	boolean add(int index, T o);
+	boolean add(int index, T o); //Adds a Song object at a specified index
 	
-	boolean add(T o);
+	boolean add(T o); //Adds a Song object to the Array
 	
-	boolean clear();
+	boolean clear(); //Removes all items from the Array
 	
-	boolean contains(T o);
+	boolean contains(T o); //Compares an object in the Array to search criteria
 	
-	T get(int index);
+	T get(int index); //Returns an index
 	
-	int indexOf(T o);
+	int indexOf(T o); //Returns the index of a specified object
 	
-	boolean isEmpty();
+	boolean isEmpty(); //Checks the Array for any objects
 	
-	T remove(int index);
+	T remove(int index); //Removes an object at an index form the Array
 	
-	T remove(T o);
+	T remove(T o); //Removes a specified object from the array
 	
-	boolean set(int index, T element);
+	boolean set(int index, T element); //sets an index for a specified object in the array list
 	
-	int size();
+	int size(); //Returns the size of the Array
 	
-	MyList subList(int fromIndex, int toIndex);
+	MyList subList(int fromIndex, int toIndex); //Creates a separate array using specified indices
 	
-	T[] toArray();
+	T[] toArray(); //Creates a new list using the original
 	
-	boolean swap(int position1, int position2);
+	boolean swap(int position1, int position2); //changes two object's positions in an array 
 	
-	boolean shift(int positions);
+	boolean shift(int positions); //Moves objects in an array to the left or right (positions) number of spaces
 		
 }
